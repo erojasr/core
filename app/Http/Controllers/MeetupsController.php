5f40f2220fe96f34998b6b9b4f556fb36f113f9f
erojasr <eduardo.utc@gmail.com>
2017-08-09 07:04:48 -0600

    meetup show by id controller

--- app/Http/Controllers/MeetupsController.php
+++ app/Http/Controllers/MeetupsController.php
@@ -20,6 +20,15 @@ class MeetupsController extends Controller
 
     }
 
+    public function getMeetups($id)
+    {
+        $meetup = Meetups::find($id);
+        if($meetup)
+            return response()->json($meetup, 200);
+
+        return response()->json(["Meetup doesn't exist"], 404);
+    }
+
     public function createNewMeetup(Request $request)
     {
         $meetup = Meetups::create($request->all());
